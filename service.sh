c92ecbf97901f12e3919da450ed9bc155dec4ece
zgt0109 <2506526350@qq.com>
2016-11-26 14:47:16 +0800

    mysql、pg等数据库软件

---
+++ service.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 管理mysql、redis、ngnix相关后台服务软件
+brew cask install launchrocket
+
+brew install mysql
+brew install postgresql
+brew install memcached
+brew install redis
+# brew install java
+brew install elasticsearch
+brew install nginx
