50ebb6b441be8c89b90897110df893d2c94a4b1d
zgt0109 <2506526350@qq.com>
2016-11-26 15:24:23 +0800

    NodeJs install

---
+++ NodeJs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# node 
+brew install node watchman
+
+# npm
+npm config set registry https://registry.npm.taobao.org
+npm install -g react-native-cli
+# npm install -g ember-cli
+npm install -g express-generator
+npm install -g bower
