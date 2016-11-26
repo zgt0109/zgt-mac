5933c232c078b21d260150c69fdd5d940c508f62
zgt0109 <2506526350@qq.com>
2016-11-26 15:54:17 +0800

    合并生成启动入口文件

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# TODO: 提示Command Line Developer Tools 是否安装
+# TODO: 提升权限需要多次输入密码
+source ./mac.sh
+source ./git.sh
+source ./baiduinput.sh
+source ./NodeJs.sh
+source ./service.sh
+source ./ruby.sh
