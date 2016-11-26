3690ff575ed00c0e8d23cd2367e0185a7d88ddf3
zgt0109 <2506526350@qq.com>
2016-11-26 14:57:49 +0800

    * 安装百度输入法 * 安装谷歌浏览器 * 安装sublime-text

---
+++ baiduinput.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 安装百度输入法
+brew cask install baiduinput && (
+  open /opt/homebrew-cask/Caskroom/baiduinput/*/安装百度输入法.app
+)
+
+# 安装谷歌浏览器
+brew cask install google-chrome
+# 安装sublime-text
+brew cask install sublime-text
